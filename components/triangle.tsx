1ad8ef44bf803483c9b0b485c745245bb62cc090
Mikk Martin <mikk.martin@gmail.com>
2024-03-06 15:53:27 +0200

    Update triangle.tsx

--- components/triangle.tsx
+++ components/triangle.tsx
@@ -9,21 +9,6 @@ export function Triangle({
     .split(" ")
     .map((point) => point.split(",").map(parseFloat));
 
-  function getCentroid(...arr: Point[]) {
-    let sumX = 0;
-    let sumY = 0;
-
-    for (let i = 0; i < arr.length; i++) {
-      sumX += arr[i].x;
-      sumY += arr[i].y;
-    }
-
-    let centroidX = sumX / arr.length;
-    let centroidY = sumY / arr.length;
-
-    return { x: centroidX.toFixed(2), y: centroidY.toFixed(2) };
-  }
-
   const midPoints = [
     getCentroid(
       { x: pointsArray[0][0], y: pointsArray[0][1] },
@@ -39,32 +24,91 @@ export function Triangle({
     ),
   ];
 
+  const midPointAngles = [
+    getLineAngle(
+      { x: pointsArray[0][0], y: pointsArray[0][1] },
+      { x: pointsArray[1][0], y: pointsArray[1][1] }
+    ),
+    getLineAngle(
+      { x: pointsArray[1][0], y: pointsArray[1][1] },
+      { x: pointsArray[2][0], y: pointsArray[2][1] }
+    ),
+    getLineAngle(
+      { x: pointsArray[2][0], y: pointsArray[2][1] },
+      { x: pointsArray[0][0], y: pointsArray[0][1] }
+    ),
+  ];
+
+  const center = getCentroid(
+    { x: pointsArray[0][0], y: pointsArray[0][1] },
+    { x: pointsArray[1][0], y: pointsArray[1][1] },
+    { x: pointsArray[2][0], y: pointsArray[2][1] }
+  );
+
   return (
     <svg viewBox="0 0 300 200" width="300" height="200">
       <polygon
         points={points}
         className="stroke stroke-2 stroke-slate-500 fill-none"
       />
-      {midPoints.map((point, i) => (
-        <>
-          <circle
-            key={i}
-            cx={point.x}
-            cy={point.y}
-            r="2"
-            className="fill-slate-500"
-          />
-          <text
-            x={point.x}
-            y={point.y}
-            className="text-xs fill-slate-500"
-            dominantBaseline="middle"
-            textAnchor="middle"
-          >
-            {i === 0 ? "A" : i === 1 ? "B" : "C"}
-          </text>
-        </>
-      ))}
+      {midPoints.map((point, i) => {
+        const textPos = findPointAtAngleAndDistance(
+          point,
+          midPointAngles[i] + Math.PI / 2,
+          16
+        );
+
+        return (
+          <>
+            <circle
+              key={i}
+              cx={point.x}
+              cy={point.y}
+              r="2"
+              className="fill-slate-500"
+            />
+            <text
+              x={textPos.x}
+              y={textPos.y}
+              className="text-xs fill-slate-500"
+              dominantBaseline="middle"
+              textAnchor="middle"
+            >
+              {i === 0 ? "A" : i === 1 ? "B" : "C"}
+            </text>
+          </>
+        );
+      })}
     </svg>
   );
+}
+
+function getLineAngle(p1: Point, p2: Point): number {
+  return Math.atan2(p2.y - p1.y, p2.x - p1.x);
+}
+
+function findPointAtAngleAndDistance(
+  centroid: Point,
+  angle: number,
+  distance: number
+): Point {
+  return {
+    x: centroid.x + distance * Math.cos(angle),
+    y: centroid.y + distance * Math.sin(angle),
+  };
+}
+
+function getCentroid(...arr: Point[]): Point {
+  let sumX = 0;
+  let sumY = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sumX += arr[i].x;
+    sumY += arr[i].y;
+  }
+
+  let centroidX = sumX / arr.length;
+  let centroidY = sumY / arr.length;
+
+  return { x: centroidX, y: centroidY };
 }
