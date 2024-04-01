40bb98a83b326e25ff4f447f719a65ed2b2d2cf3
Mikk Martin <mikk.martin@gmail.com>
2024-04-01 09:39:31 +0300

    Update triangle.tsx

--- pages/api/triangle.tsx
+++ pages/api/triangle.tsx
@@ -1,10 +1,9 @@
-import { triangleDrawPrompt } from "@/app/ai-function-prompts";
 import { Triangle } from "@/components/triangle";
 import type { NextApiRequest, NextApiResponse } from "next";
 import ReactDOMServer from "react-dom/server";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const object = {
+  const props = {
     points: (req.query.points as string) || "20,150 250,150 275,50",
     corners: (req.query.corners as string | undefined)?.split(","),
     angles: (req.query.angles as string | undefined)
@@ -14,13 +13,6 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       ?.split(",")
       .map(safeParseJson),
   };
-
-  const result = triangleDrawPrompt.parameters.safeParse(object);
-  if (!result.success) {
-    return res.status(400).json(result.error);
-  }
-
-  const props = result.data;
   const markup = ReactDOMServer.renderToStaticMarkup(<Triangle {...props} />);
 
   res.setHeader("Content-Type", "image/svg+xml");
