344fd76359365c8d072d59430aecd0224fed74be
Mikk Martin <mikk.martin@gmail.com>
2024-06-07 11:12:55 +0300

    Update page.tsx

--- app/test2/page.tsx
+++ app/test2/page.tsx
@@ -7,7 +7,9 @@ import { z } from "zod";
 type Diagonals = z.infer<(typeof cuboidDrawPrompt)["parameters"]>["diagonals"];
 
 export default function Test2() {
-  const diagonals: Diagonals = [];
+  const [props, setProps] = useState<CuboidProps>({
+    size: [2, 1],
+  });
 
   function getSideValue(side: string | undefined): boolean | string {
     if (side === "") return true;
@@ -18,111 +20,254 @@ export default function Test2() {
   return (
     <div className="mx-auto w-full max-w-lg my-16 bg-[#F3F2F0] p-2 rounded-3xl">
       <div className="bg-white flex justify-center items-center p-2 rounded-2xl">
-        <Cuboid size={[2, 1]} diagonals={diagonals} />
+        <Cuboid {...props} />
       </div>
       <div className="flex gap-1 p-2">
         <h2>Ruuttahukas</h2>
-        <Prop title="Sides">
-          {["width", "depth", "height"].map((side) => (
-            <fieldset className="flex gap-5 items-center">
-              <label
-                className="text-[13px] text-violet11 w-[75px]"
-                htmlFor={side}
-              >
-                {side}
-              </label>
-              <input type="checkbox" id={side} />
-              <input
-                className="w-full inline-flex items-center justify-center flex-1 rounded px-2.5 text-[13px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 h-[25px] focus:shadow-[0_0_0_2px] focus:shadow-violet8 outline-none"
-                id={side}
-                placeholder="10 cm"
-              />
-            </fieldset>
-          ))}
-        </Prop>
-        <Prop title="Corners">
-          {[
-            {
-              label: "top",
-              inputs: [
-                { label: "TL", value: "" },
-                { label: "TR", value: "" },
-                { label: "BL", value: "" },
-                { label: "BR", value: "" },
-              ],
-            },
-            {
-              label: "bottom",
-              inputs: [
-                { label: "TL", value: "" },
-                { label: "TR", value: "" },
-                { label: "BL", value: "" },
-                { label: "BR", value: "" },
-              ],
-            },
-          ].map(({ label, inputs }) => (
-            <div className="grid grid-cols-6 gap-1">
-              <p className="col-span-2">{label}</p>
-              {inputs.map(({ label, value }) => (
-                <fieldset>
-                  <input
-                    className="w-full inline-flex items-center justify-center flex-1 rounded px-2.5 text-[13px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 h-[25px] focus:shadow-[0_0_0_2px] focus:shadow-violet8 outline-none text-center"
-                    id={label}
-                    placeholder="A"
-                  />
-                  <label
-                    className="text-[12px] text-slate-400 text-center w-full inline-block"
-                    htmlFor={label}
-                  >
-                    {label}
-                  </label>
-                </fieldset>
-              ))}
-            </div>
-          ))}
-        </Prop>
-        <Prop title="Diagonals">
-          {[{ label: "top" }, { label: "bottom" }].map((corner) => (
-            <fieldset className="flex gap-5 items-center">
-              <label
-                className="text-[13px] text-violet11 w-[75px]"
-                htmlFor={corner.label}
-              >
-                {corner.label}
-              </label>
-              <input type="checkbox" checked id={corner.label} />
-              <input
-                className="w-full inline-flex items-center justify-center flex-1 rounded px-2.5 text-[13px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 h-[25px] focus:shadow-[0_0_0_2px] focus:shadow-violet8 outline-none"
-                id={corner.label}
-                placeholder="10 cm"
-              />
-            </fieldset>
-          ))}
-        </Prop>
+        <PropsEditor props={props} onChange={setProps} />
       </div>
-      <pre>{JSON.stringify(diagonals, null, 2)}</pre>
+      <pre className="text-xs">{JSON.stringify(props, null, 2)}</pre>
     </div>
   );
 }
 
-import * as Popover from "@radix-ui/react-popover";
+type CuboidProps = z.infer<(typeof cuboidDrawPrompt)["parameters"]>;
 
-const Prop = ({
+function PropsEditor({
+  props,
+  onChange,
+}: {
+  props: CuboidProps;
+  onChange: (props: CuboidProps) => void;
+}) {
+  const sidesEnabled = Array.isArray(props.sides);
+  const [values, setValues] = useState<{
+    [key: string]: { enabled: boolean; value?: string };
+  }>({
+    width: { enabled: true },
+    depth: { enabled: true },
+    height: { enabled: true },
+  });
+
+  function getSideValues(newState?: typeof values): CuboidProps["sides"] {
+    const _values = newState ?? values;
+    return Object.values(_values).map(({ enabled, value }) => {
+      if (!enabled) return false;
+      return typeof value === "string" ? value : true;
+    });
+  }
+
+  return (
+    <>
+      <PopoverEditor
+        title="Sides"
+        enabled={sidesEnabled}
+        onClick={() =>
+          !sidesEnabled && onChange({ ...props, sides: getSideValues() })
+        }
+        onDisable={() => onChange({ ...props, sides: undefined })}
+      >
+        {Object.entries(values).map(([side, { enabled, value }]) => (
+          <fieldset className="flex gap-5 items-center">
+            <label
+              className="text-[13px] text-violet11 w-[75px]"
+              htmlFor={side}
+            >
+              {side}
+            </label>
+            <input
+              type="checkbox"
+              id={side}
+              checked={enabled}
+              onClick={() => {
+                setValues((values) => {
+                  const newState = {
+                    ...values,
+                    [side]: { enabled: !enabled, value },
+                  };
+                  onChange({ ...props, sides: getSideValues(newState) });
+                  return newState;
+                });
+              }}
+            />
+            <input
+              className="w-full inline-flex items-center justify-center flex-1 rounded px-2.5 text-[13px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 h-[25px] focus:shadow-[0_0_0_2px] focus:shadow-violet8 outline-none"
+              id={side}
+              disabled={!enabled}
+              value={value}
+              onChange={(e) => {
+                setValues((values) => {
+                  const newState = {
+                    ...values,
+                    [side]: { enabled: true, value: e.target.value },
+                  };
+                  onChange({ ...props, sides: getSideValues(newState) });
+                  return newState;
+                });
+              }}
+              placeholder="10 cm"
+            />
+          </fieldset>
+        ))}
+      </PopoverEditor>
+      <PopoverEditor title="Corners">
+        {[
+          {
+            label: "top",
+            inputs: [
+              { label: "TL", value: "" },
+              { label: "TR", value: "" },
+              { label: "BL", value: "" },
+              { label: "BR", value: "" },
+            ],
+          },
+          {
+            label: "bottom",
+            inputs: [
+              { label: "TL", value: "" },
+              { label: "TR", value: "" },
+              { label: "BL", value: "" },
+              { label: "BR", value: "" },
+            ],
+          },
+        ].map(({ label, inputs }) => (
+          <div className="grid grid-cols-6 gap-1">
+            <p className="col-span-2">{label}</p>
+            {inputs.map(({ label, value }) => (
+              <fieldset>
+                <input
+                  className="w-full inline-flex items-center justify-center flex-1 rounded px-2.5 text-[13px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 h-[25px] focus:shadow-[0_0_0_2px] focus:shadow-violet8 outline-none text-center"
+                  id={label}
+                  placeholder="A"
+                />
+                <label
+                  className="text-[12px] text-slate-400 text-center w-full inline-block"
+                  htmlFor={label}
+                >
+                  {label}
+                </label>
+              </fieldset>
+            ))}
+          </div>
+        ))}
+      </PopoverEditor>
+      <PopoverEditor title="Diagonals">
+        {[{ label: "top" }, { label: "bottom" }].map((corner) => (
+          <fieldset className="flex gap-5 items-center">
+            <label
+              className="text-[13px] text-violet11 w-[75px]"
+              htmlFor={corner.label}
+            >
+              {corner.label}
+            </label>
+            <input type="checkbox" checked id={corner.label} />
+            <input
+              className="w-full inline-flex items-center justify-center flex-1 rounded px-2.5 text-[13px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 h-[25px] focus:shadow-[0_0_0_2px] focus:shadow-violet8 outline-none"
+              id={corner.label}
+              placeholder="10 cm"
+            />
+          </fieldset>
+        ))}
+      </PopoverEditor>
+    </>
+  );
+}
+
+function SidesContent({ enabled, setEnabled }: any) {
+  const [values, setValues] = useState<{
+    [key: string]: { enabled: boolean; value?: string };
+  }>({
+    width: { enabled: true },
+    depth: { enabled: true },
+    height: { enabled: true },
+  });
+
+  return (
+    <PopoverEditor
+      title="Sides"
+      enabled={enabled}
+      onClick={() => !enabled && setEnabled(true)}
+      onDisable={() => setEnabled(false)}
+    >
+      {Object.entries(values).map(([side, { enabled, value }]) => (
+        <fieldset className="flex gap-5 items-center">
+          <label className="text-[13px] text-violet11 w-[75px]" htmlFor={side}>
+            {side}
+          </label>
+          <input
+            type="checkbox"
+            id={side}
+            checked={enabled}
+            onClick={() => {
+              setValues((values) => ({
+                ...values,
+                [side]: {
+                  enabled: !enabled,
+                  value,
+                },
+              }));
+            }}
+          />
+          <input
+            className="w-full inline-flex items-center justify-center flex-1 rounded px-2.5 text-[13px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 h-[25px] focus:shadow-[0_0_0_2px] focus:shadow-violet8 outline-none"
+            id={side}
+            disabled={!enabled}
+            placeholder="10 cm"
+          />
+        </fieldset>
+      ))}
+    </PopoverEditor>
+  );
+}
+
+import * as Popover from "@radix-ui/react-popover";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import { AnimatePresence, motion } from "framer-motion";
+
+const PopoverEditor = ({
   title,
   children,
+  onClick,
+  enabled,
+  onDisable,
 }: {
   title: string;
   children: React.ReactNode;
+  onClick?: () => void;
+  enabled?: boolean;
+  onDisable?: () => void;
 }) => (
   <Popover.Root>
-    <Popover.Trigger asChild>
-      <button
-        className="rounded-full bg-black/5 text-slate-600 tracking-wide uppercase text-xs font-medium px-3 py-1 text-violet11"
-        aria-label="Update dimensions"
+    <motion.div
+      layout
+      style={{
+        borderRadius: 99,
+      }}
+      className={cn(
+        "flex bg-black/5 text-slate-600 tracking-wider text-xs h-6 font-medium",
+        enabled && "bg-white"
+      )}
+    >
+      <Popover.Trigger
+        className={cn("uppercase px-3 py-1", enabled && "pr-0")}
+        onClick={onClick}
+        asChild
       >
-        {title}
-      </button>
-    </Popover.Trigger>
+        <motion.button layout transition={{ duration: 0 }}>
+          {title}
+        </motion.button>
+      </Popover.Trigger>
+      {enabled && (
+        <button
+          className="inline-block w-6 h-6 rounded-e-full hover:bg-slate-100"
+          onClick={onDisable}
+        >
+          x
+        </button>
+      )}
+    </motion.div>
+
     <Popover.Portal>
       <Popover.Content
         className="rounded p-5 w-[260px] bg-white  will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade"
