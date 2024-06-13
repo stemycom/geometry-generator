da7f602d6c836d214f7b8327145e8c567bf52976
Mikk Martin <mikk.martin@gmail.com>
2024-06-13 11:04:22 +0300

    Update dropdown.tsx

--- components/ui/dropdown.tsx
+++ components/ui/dropdown.tsx
@@ -50,6 +50,7 @@ const DropdownMenuSubContent = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DropdownMenuPrimitive.SubContent
     ref={ref}
+    alignOffset={-8}
     className={cn(
       "z-50 min-w-[8rem] overflow-hidden rounded-[1.25rem] bg-popover p-2 text-popover-foreground shadow-[0_10px_60px_-10px_rgba(0,0,0,0.2)] data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
       className
